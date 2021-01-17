9e2caa35f416117210f3bcf9d7624078ed4fadc6
jleahred <jleahred@gmail.com>
2021-01-17 14:06:44 +0100

    removed indirection on actions calls

--- src/gen_files/cpp/templates/cpp_gen_file.rs
+++ src/gen_files/cpp/templates/cpp_gen_file.rs
@@ -74,7 +74,7 @@ SState {{status.name}}::input(const in_{{input}}_t& in) {
         {
           log("[{{status.name}}] {{sinput.name}} -> {{transition.new_status.name}}", in, info, nw_st_info);
           {% for action in transition.actions -%}
-          act_{{action}}(this->info, in, *nw_st_info);
+          act_{{action}}(this->info, in, nw_st_info);
           {% endfor %}
           return std::make_shared<{{transition.new_status.name}}>(*nw_st_info);
         } else if(auto nw_st_info = std::get_if<st_error_t>(&nw_st_info_or_error)){
@@ -85,7 +85,7 @@ SState {{status.name}}::input(const in_{{input}}_t& in) {
         auto nw_st_info = fromin2_error<st_{{status.name}}_t, in_{{sinput.name}}_t>(this->info, in);
         log("[{{status.name}}] {{sinput.name}} -> {{transition.new_status.name}}", in, info, nw_st_info);
         {% for action in transition.actions -%}
-        act_{{action}}(this->info, in, *nw_st_info);
+        act_{{action}}(this->info, in, nw_st_info);
         {% endfor %}
         return std::make_shared<{{transition.new_status.name}}>(nw_st_info);
         {% endif %}
